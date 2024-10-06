8700af4a90dcd798e959b54ab0a5adc25013d501
Zoltan Ersek <zoltan.ersek@bp.com>
2024-10-06 22:23:11 +0300

    part2c exercises

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios";
 
 const Filter = ({filter, setFilter}) => (
     <div>
@@ -27,12 +28,7 @@ const Persons = ({ persons }) =>
 
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456'},
-        {name: 'Ada Lovelace', number: '39-44-5323523'},
-        {name: 'Dan Abramov', number: '12-43-234345'},
-        {name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
@@ -55,6 +51,13 @@ const App = () => {
     }
 
     const personsToShow = persons.filter(person => filter === '' ? true : person.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons")
+            .then(res => {
+                setPersons(res.data)
+            })
+    }, []);
 
     return (
         <div>
